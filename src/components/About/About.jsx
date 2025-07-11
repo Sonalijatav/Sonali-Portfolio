02a9b07e1dde8ba1822bb2c5cc9a1c9470188c99
Sonali Jatav <sonalijatav972@gmail.com>
2025-07-12 00:23:51 +0530

    resume add

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -48,7 +48,7 @@ const About = () => {
           </p>
           {/* Resume Button */}
           <a
-            href="https://drive.google.com/file/d/1hvrgbNeVhaG_xVQ4vvcMZbOzxEeVlbhw/view?usp=sharing"
+            href="https://drive.google.com/file/d/15cLEbULQxlz0406GvYkzbMcob4xAQOqU/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="inline-block text-white py-3 px-8 rounded-full mt-5 text-lg font-bold transition duration-300 transform hover:scale-105"
